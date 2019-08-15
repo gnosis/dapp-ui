39f7e8c51ee6bc0d004dba2ff2e837c824992276
David Sato <david.sato64@gmail.com>
2019-08-15 16:48:23 +0200

    removed unnecessary code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { Global } from 'styles'
 
-import AppRoutes from 'routes/'
-
 const App: React.FC = () => (
     // <Providers>
     <>
         <Global />
-        <AppRoutes />
     </>
     // </Providers>
 )
