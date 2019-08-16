85e83caa67f9a2de4d822a089f8a4c784d4541f5
David Sato <david.sato64@gmail.com>
2019-08-16 14:53:37 +0200

    package index file with exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
-import 'react-hot-loader'
+// API Functions
+import getWeb3API, { Web3SetupOptions, Web3FrontendAPI } from './api/web3'
+// Components
+import Web3ConnectButton from './components/Web3ConnectButton'
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+/**
+ * setupGnosisUI
+ * @desc Set up function to init web3 frontend API and do anything else
+ * W.I.P
+ * @param { Web3SetupOptions } options
+ */
+async function setupGnosisUI(options: Web3SetupOptions = {}): Promise<Web3FrontendAPI | Error> {
+    try {
+        return getWeb3API(options)
+    } catch (error) {
+        console.error('setupGnosisUI Error!', error)
+        throw new Error(error)
+    }
+}
 
-import App from 'components/App'
-
-const root = document.getElementById('root')
-ReactDOM.render(<App />, root)
+export {
+    // Setup Fns
+    getWeb3API,
+    setupGnosisUI,
+    // Components
+    Web3ConnectButton,
+    // Types
+    Web3FrontendAPI,
+}
