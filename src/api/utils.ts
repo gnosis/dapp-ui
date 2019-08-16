5a2bc3848700b047e5f0373a3154bad95572d76f
David Sato <david.sato64@gmail.com>
2019-08-16 14:43:07 +0200

    API utils file

---
+++ src/api/utils.ts
@@ -0,0 +1,20 @@
+export const windowLoaded = new Promise((accept, reject) => {
+    if (typeof window === 'undefined') {
+        return accept()
+    }
+
+    if (typeof window.addEventListener !== 'function') {
+        return reject(new Error('expected to be able to register event listener'))
+    }
+
+    window.addEventListener(
+        'load',
+        function loadHandler(event) {
+            window.removeEventListener('load', loadHandler, false)
+
+            // return setTimeout(() => accept(event), 2000)
+            return accept(event)
+        },
+        false,
+    )
+})
