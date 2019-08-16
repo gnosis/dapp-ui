3ba5072c413375c684b67fb1187cfccf8da9a2ff
David Sato <david.sato64@gmail.com>
2019-08-16 14:53:05 +0200

    Web3ConnectButton

--- src/components/Web3ConnectButton.tsx
+++ src/components/Web3ConnectButton.tsx
@@ -1,30 +1,37 @@
 import React, { useState } from 'react'
-
 import Web3Connect from 'web3connect'
-import { provider as ProviderType } from 'web3-providers'
+// API
 import { getWeb3API, Web3FrontendAPI } from '../api/web3'
-
-import DisplayWeb3 from './Test_DisplayWeb3'
+// Components
+import DisplayWeb3 from './DisplayWeb3'
+// TS Types
+import { provider as ProviderType } from 'web3-providers'
 
 const Web3ConnectButton: React.FC = () => {
-    const [web3, setWeb3] = useState<Web3FrontendAPI>()
+    const [web3FrontendAPI, setWeb3FrontendAPI] = useState<Web3FrontendAPI>()
 
     return (
         <>
             <Web3Connect.Button
+                // providerOptions={{
+                //     walletconnect: {
+                //         bridge: 'https://safe-walletconnect.gnosis.io',
+                //     },
+                // }}
                 onConnect={async (provider: ProviderType): Promise<void> => {
                     console.log(
                         'Web3Connect.Button::Connected to',
                         typeof provider === 'object' ? provider.constructor.name : provider,
                     )
-                    const web3 = await getWeb3API({ customProvider: provider }) // add provider to web3
-                    setWeb3(web3)
+                    const web3FrontendAPI = await getWeb3API({ customProvider: provider }) // add provider to web3FrontendAPI
+                    setWeb3FrontendAPI(web3FrontendAPI)
                 }}
                 onClose={(): void => {
                     console.log('Web3Connect Modal Closed') // modal has closed
                 }}
+                label={web3FrontendAPI ? 'Change Wallet' : 'Connect'}
             />
-            {web3 && <DisplayWeb3 web3={web3} />}
+            {web3FrontendAPI && <DisplayWeb3 web3FrontendAPI={web3FrontendAPI} />}
         </>
     )
 }
