4daab532af1d50fc69c4b3772624779cddc36a02
David Sato <david.sato64@gmail.com>
2019-08-16 14:43:39 +0200

    global TS typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,11 @@
 declare module '@walletconnect/web3-provider'
+
+declare global {
+    interface Window {
+        ethereum: provider & {
+            enable: () => Promise<string[]>
+            on?: () => EventListener
+        }
+        web3: Web3
+    }
+}
