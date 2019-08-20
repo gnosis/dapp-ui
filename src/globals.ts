7d536dbf5b0f5d75e473c7690642ee14e37624bc
David Sato <david.sato64@gmail.com>
2019-08-20 10:58:34 +0200

    types + globals file creation
    1. global file with constants
    2. types file (global)
    3. removed typings.d.ts + old utils file

---
+++ src/globals.ts
@@ -0,0 +1,16 @@
+export const NETWORK_NAMES = {
+    MAINNET: 'MAINNET',
+    ROPSTEN: 'ROPSTEN',
+    KOVAN: 'KOVAN',
+    RINKEBY: 'RINKEBY',
+    GOERLI: 'GOERLI',
+}
+
+export const NETWORK_ID_TO_NAME = {
+    1: 'MAINNET',
+    3: 'ROPSTEN',
+    4: 'RINKEBY',
+    5: 'GOERLI',
+    42: 'KOVAN',
+    1337: 'PRIVATE',
+}
