14e0a823734cd19ff19469f20919428acb985627
David Sato <david.sato64@gmail.com>
2019-08-21 14:09:28 +0200

    example fe: choose between button | manual

--- examples/ts-frontend/src/index.tsx
+++ examples/ts-frontend/src/index.tsx
@@ -1,15 +1,30 @@
-import React from '../../../node_modules/react'
+import React, { ChangeEvent, useState } from '../../../node_modules/react'
 import ReactDOM from '../../../node_modules/react-dom'
 
-import { Web3ConnectButton } from '../../../src'
+import { Web3ConnectButton, Web3ConnectManual } from '../../../src'
 
-const Hello: React.FC = () => (
-    <div>
-        <h1>Hello World!</h1>
-        <h2>Connect wallet</h2>
-        <Web3ConnectButton />
-    </div>
-)
+const Hello: React.FC = () => {
+    const [walletConnectType, setWalletConnectType] = useState<string>(null)
+
+    return (
+        <div>
+            <h1>Connecting options:</h1>
+            <select
+                id="wallet-type"
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => setWalletConnectType(e.target.value)}
+            >
+                <option value="">--Select Wallet Connect Type--</option>
+                <option value="MANUAL">Manual</option>
+                <option value="BUTTON">Button</option>
+            </select>
+            {walletConnectType === 'BUTTON' ? (
+                <Web3ConnectButton />
+            ) : walletConnectType === 'MANUAL' ? (
+                <Web3ConnectManual />
+            ) : null}
+        </div>
+    )
+}
 
 var mountNode = document.getElementById('app')
 ReactDOM.render(<Hello />, mountNode)
