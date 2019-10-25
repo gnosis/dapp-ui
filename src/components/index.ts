e1ff7f6b75da523ab5f4c71fc957c27a3eedae6e
Velenir <velenir@gmail.com>
2019-10-25 13:37:38 +0300

    remove React-dependent code

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import Web3ConnectButton from './WalletIntegration/Web3ConnectButton'
-import Web3ConnectManual from './WalletIntegration/Web3ConnectManual'
-
-export { Web3ConnectButton, Web3ConnectManual }
