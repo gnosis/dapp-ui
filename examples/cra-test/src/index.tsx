9e482ae09cf666078170c327c1622359c21c5bc4
Velenir <velenir@gmail.com>
2019-10-25 13:50:37 +0300

    main App component

--- examples/cra-test/src/index.tsx
+++ examples/cra-test/src/index.tsx
@@ -1,39 +1,12 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { Web3ConnectButton, Web3ConnectManual } from '@gnosis.pm/dapp-ui'
-
-// const Hello: React.FC = () => {
-//     const [state] = useState<string>('STRING')
-
-//     return (
-//         <div>
-//             <h1>Connecting options:</h1>
-//             <Comp />
-//             {state}
-//         </div>
-//     )
-// }
+import ConnectButton from './components/ConnectButton'
 
 const Hello: React.FC = () => {
-    const [walletConnectType, setWalletConnectType] = useState<string>('')
-
     return (
         <div>
-            <h1>Connecting options:</h1>
-            <select
-                id="wallet-type"
-                onChange={(e) => setWalletConnectType(e.target.value)}
-            >
-                <option value="">--Select Wallet Connect Type--</option>
-                <option value="MANUAL">Manual</option>
-                <option value="BUTTON">Button</option>
-            </select>
-            {walletConnectType === 'BUTTON' ? (
-                <Web3ConnectButton />
-            ) : walletConnectType === 'MANUAL' ? (
-                <Web3ConnectManual />
-            ) : null}
+            <ConnectButton />
         </div>
     )
 }
